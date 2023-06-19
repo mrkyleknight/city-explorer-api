bb86db0715b0ebf957fdb96d3b6876fba5a4ff33
Kyle Knight <knightkyle@outlook.com>
2023-06-18 18:45:46 -0700

    Updated backend lab 8

--- server.js
+++ server.js
@@ -83,12 +83,12 @@ app.get('/weather', async (request, response, next) => {
   }
 });
 
-app.get('/movies', async (request, response, next)=> { 
+app.get('/movie', async (request, response, next)=> { 
   try {
 
-    let keywordFromFrontend = request.query.searchQuery;
+    let city = request.query.searchQuery;
 
-    let url = `https://api.themovieb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${keywordFrontend}`;
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${city}`;
 
     let dataFromAxios = await axios.get(url);
 
@@ -101,8 +101,16 @@ app.get('/movies', async (request, response, next)=> {
 });
 
 class Movie {
-constructor(movieObj) {
-  this.title = movieObj.title;
-  this.overview = movieObj.title;
+  constructor(movie) {
+    this.title = movie.title;
+    this.overview = movie.overview;
+    this.averageVotes = movie.vote_average;
+    this.totalVotes = movie.vote_count;
+    this.imageUrl = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
+    this.popularity = movie.popularity;
+    this.releasedOn = movie.release_date;
+    this.timestamp = Date.now();
+  }
 }
-}
+
+
